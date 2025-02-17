3ffbba6bc7c644878eb00854da5f5e54ee6eebc8
dragoon125 <santiprime2@gmail.com>
2025-02-16 23:52:19 -0500

    Add files via upload

---
+++ main/PA2.py
@@ -0,0 +1,100 @@
+#import numpy as np
+
+# Neville's method
+#x =  [1, 1.3, 1.6, 1.9, 2.2] #notes
+#val = [.7651977, .6200860, .4554022, .2818186, .1103623]
+#w = 1.5
+x = [3.6, 3.8, 3.9]
+val = [1.675, 1.436, 1.318]
+w = 3.7
+
+
+neville = [[0.0 for _ in range(len(x))] for _ in range(len(x))]
+
+for i in range(len(x)):
+    neville [i][0] = val[i]
+
+for i in range(1, len(x)):
+    for j in range(1, i+1):
+        term1 = (w - x[i - j]) * neville[i][j - 1]
+        term2 = (w - x[i]) * neville[i - 1][j - 1]
+
+        neville[i][j] = (term1 - term2) / (x[i] - x[i - j])
+
+for i in range(len(x)):
+    for j in range(i + 1):
+        #print(format(neville[i][j], '.7f'), end=' ')
+        if(i == 1):
+            if(j == i):
+                print(format(neville[i][j]))
+    #print()
+
+print()
+
+# Newton's forward method
+#xi = [1, 1.3, 1.6, 1.9, 2.2] #notes
+#fxi = [.7651977, .6200860, .4554022, .2818186, .1103623]
+xi = [7.2, 7.4, 7.5, 7.6]
+fxi = [23.5492, 25.3913, 26.8224, 27.4589]
+
+lim = len(xi)
+diffs = [[0.0 for _ in range(lim)] for _ in range(lim)]
+
+for i in range(lim):
+    diffs[i][0] = fxi[i]
+
+for i in range(1, lim):
+    for j in range(1, i + 1):
+        diffs[i][j] = (diffs[i][j-1] - diffs[i-1][j-1]) / (xi[i] - xi[i - j])
+
+#print('divided difference table:')
+for i in range(lim):
+    print('[', end='')
+    for j in range(i + 1):
+        if diffs[i][j] >= 0:
+            print(end=' ')
+        print(format(diffs[i][j], '.7f'), end=' ')
+    print(']')
+
+print()
+    
+# Divided difference Hermite Polynomial approximation matrix
+#x = [3.6, 3.8, 3.9]
+#fx = [1.675, 1.436, 1.318]
+#fxi = [-1.195, -1.188, -1.182] #my own thing
+x = [1.3, 1.3, 1.6, 1.6, 1.9, 1.9]
+fx = [0.6200860, 0.6200860, 0.4554022, 0.4554022, 0.2818186, 0.2818186]
+fxi = [-0.5220232, -0.5220232, -0.5698959, -0.5698959, -0.5811571, -0.5811571]
+
+lim = len(x)
+diffs = [[0.0 for _ in range(lim)] for _ in range(lim)]
+
+for i in range(lim):
+    diffs[i][0] = x[i]
+
+for i in range(lim):
+    diffs[i][1] = fx[i]
+
+for i in range(lim):
+    for j in range (2):
+        print(diffs[i][j], end=' ')
+    print();
+
+count = 0
+
+# cubic spline interpolation
+
+temp = [
+        [1.0,  0.0,   0.0,   0.0],
+        [3.0, 12.0,   3.0,   0.0],
+        [0.0,  3.0,  10.0,   2.0],
+        [0.0,  0.0,   0.0,   1.0],[0.0,  0.0,   1.0,   0.0], [0.0, -0.02702703, 0.10810811, 0.0]
+    ]
+
+for row in temp:
+    print("[", end="")
+    for val in row:
+        print(f"{val: .0f}", end="")
+        if val != row[-1]:
+            print(" ", end="")
+    print("]")
